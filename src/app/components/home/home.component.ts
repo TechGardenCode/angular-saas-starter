8179e9f3b8ababaed7394f7055dba1bbdf41c9b9
techgardencode <techgardencode@gmail.com>
2024-09-17 13:39:22 -0400

    feat: add default templates + branding

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  commands = [
+    'git clone https://github.com/techgardencode/angular-saas-starter',
+    'npm install',
+    'npm run start',
+  ];
 
+  copyToClipboard(commandList: string[]) {
+    navigator.clipboard.writeText(commandList.join('\n'));
+  }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from '../footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
